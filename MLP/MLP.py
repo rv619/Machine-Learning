1db5835421b3b72a7baede23c8e2f459370e26f5
rv619 <45069643+rv619@users.noreply.github.com>
2021-07-14 11:48:12 +0530

    Add files via upload

---
+++ MLP/MLP.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import copy, deepcopy
+
+def split (dataset, training_ratio):
+    n = len (dataset) # Size of dataset
+    r = int (training_ratio * n) # No. of training examples
+    np.random.shuffle (dataset) # Shuffling the dataset
+    return dataset[:r,:], dataset[r:,:]
+
+def phi (x, a = 1): # Activation function
+    return (1 / (1 + np.exp((-a) * x))) #sigmoid
+    
+def phi_dash (x, a = 1): # Derivative of activation function
+    phi_x = phi (x, a)
+    return a * phi_x * (1 - phi_x)
+
+def feed_forward (x, w, h, bias):
+    u = np.array ([None]*(h+2))
+    phi_u = np.array ([None]*(h+2))
+    phi_dash_u = np.array ([None]*(h+2))
+    u[0] = np.insert (deepcopy (x), 0, bias, axis = 1)
+    phi_u[0] = u[0]
+    for i in range (h+1):
+        u[i+1] = np.dot (phi_u[i], np.transpose (w[i]))
+        phi_dash_u[i+1] = phi_dash (u[i+1]) # No bias yet
+        # Add bias
+        if i != h:
+            u[i+1] = np.insert (u[i+1], 0, bias, axis = 1)
+        phi_u[i+1] = phi (u[i+1])
+    return phi_u, phi_dash_u
+    
+def back_propagate (error, w, phi_dash_u, h):
+    delta = np.array ([None]*(h+1))
+    delta[h] = np.multiply (error, phi_dash_u[h+1])
+    for i in range (h-1,-1,-1):
+        del_cur = np.dot (delta[i+1], w[i+1])
+        del_cur = np.delete (del_cur, 0, axis = 1)
+        delta[i] = np.multiply (phi_dash_u[i+1], del_cur)
+    return delta
+    
+def update_weights (w, phi_u, delta, h, learning_rate):
+    for i in range (h+1):
+        del_w = np.dot (np.transpose (delta[i]), phi_u[i])
+        w[i] += learning_rate * del_w
+    return w
+    
+def train (train_data, hidden, h, d, c, class_mapping, bias, learning_rate = 0.1, eps = 0.001, epoch = 10000):
+    n = len (train_data) # Size of training dataset
+    # Split the data
+    x, y = train_data[:,:d], train_data[:,d:]
+    it = 0
+    w = np.array ([None]*(h+1))
+    dimensions = hidden.copy()
+    dimensions.insert (0, d)
+    dimensions.append (c)
+    w = np.array ([None]*(h+1))
+    for i in range (h+1):
+        w[i] = np.random.random((dimensions[i+1], dimensions[i]+1))
+    while it < 10000:
+        E = 0
+        for i in range (n):
+            x_cur = x[i:i+1,:]
+            phi_u, phi_dash_u = feed_forward (x_cur, w, h, bias)
+            y_mask = np.array ([0] * c) # one-hot representation
+            y_mask [class_mapping[y[i,0]]] = 1
+            error = y_mask - phi_u[h+1]
+            delta = back_propagate (error, w, phi_dash_u, h)
+            w = update_weights (w, phi_u, delta, h, learning_rate)
+            e = 0.5 * np.sum (np.multiply (error, error))
+            E += e / n
+        if E < eps:
+            break
+        it += 1
+    return w
+    
+def test (test_data, w, h, d, c, class_mapping, bias):
+    n = len (test_data) # Size of testing dataset
+    # Split the data
+    x, y = test_data[:,:d], test_data[:,d:]
+    phi_u, phi_dash_u = feed_forward (x, w, h, bias)
+    y_pred = phi_u[h+1].argmax (axis = 1)
+    mismatched = 0
+    for i in range (n):
+        mismatched += abs (class_mapping [y[i,0]] - y_pred[i])
+    accuracy = ((n - mismatched) / n) * 100
+    print ("Accuracy = " + str(round(accuracy, 2)))
+    
+def run (filename, hidden, h, d, c, classes, bias = 1.0):
+    # Read the csv file
+    dataset = pd.read_csv (filename).to_numpy()
+    # Split the data set
+    train_data, test_data = split (dataset, 0.8)
+    # Map classes
+    class_mapping = {}
+    for i in range (len(classes)):
+        class_mapping [classes[i]] = i
+    # Train the data
+    w = train (train_data, hidden, h, d, c, class_mapping, bias)
+    # Display weights
+    for i in range (h+1):
+        print ("w [" + str(i) + "] = \n")
+        print (w[i])
+        print ()
+    # Test the data
+    test (test_data, w, h, d, c, class_mapping, bias)
+
+def main ():
+    file1 = 'MLP1.csv'
+    file2 = 'MLP2.csv'
+    d = 2 # no. of features
+    c = 2 # no. of classes
+    h = 1 # no. of hidden layers
+    hidden = [4] # no. of nodes in hidden layers 
+    print ("----------------------------------------------")
+    print ("Results (" + file1 + ")")
+    print ("----------------------------------------------\n")
+    classes = [1, 2]
+    run (file1, hidden, h, d, c, classes)
+    print ("\n----------------------------------------------")
+    print ("Results (" + file2 + ")")
+    print ("----------------------------------------------\n")
+    classes = [-1, 1]
+    run (file2, hidden, h, d, c, classes)
+    print ("\n----------------------------------------------")
+
+main()
---
+++ MLP/README.txt
@@ -0,0 +1,26 @@
+This is a multi-layer perceptron (MLP) machine learning model.
+ 
+It has been tested and trained on the datasets present in 
+"MLP1.csv" and "MLP2.csv" files.
+
+The main python code is in "MLP.py" file.
+Test-Train split is randomized (20:80).
+
+The code is generalized for 'd' features, 'c' classes, 
+and 'h' hidden layers.
+
+Pandas, Mathplotlib, and Numpy libraries are essential 
+for the code to run.
+
+A screenshot of the output (test results) can be found in 
+'MLP_Test_Data_Output.png' file.
+
+There is another python file - 'MLP_Logic_Gates.py' that tests
+the model on AND, OR, XOR logic gates.
+
+A screenshot of the output of the above python code can be
+found in 'Logic_Gates_Output_Screenshot.png' file.
+
+As evident from the output: -
+  -> MLP with a single hidden layer works perfectly on 
+     ALL three gates (100% accuracy, each). 
